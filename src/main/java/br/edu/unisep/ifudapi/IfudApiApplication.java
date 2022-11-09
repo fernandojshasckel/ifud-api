0cca871b05f62c2af0150839e3f40295ae55cfa6
fernando.hasckel <fernando.hasckel@ciss.com.br>
2022-11-08 23:57:32 -0300

    001 - Criação do projeto

---
+++ src/main/java/br/edu/unisep/ifudapi/IfudApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unisep.ifudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unisep/ifudapi/IfudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unisep.ifudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
